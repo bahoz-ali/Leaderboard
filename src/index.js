8fec3acee9060292aefa2765cf5af43e0e12a22c
Bahoz <bahozali50@gmail.com>
2022-07-21 15:35:38 +0300

    implement both createNewScore() and getAllscores() function in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import './style.css';
 
+const addScoreBtn = document.querySelector('add_score');
+const userInput = document.querySelector('user_input');
+const scoreInput = document.querySelector('score_input');
+
 const baseUrl =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 
 const game = {
   name: 'tower books',
   gameId: 'CLwRs3Mgq5HE7kyDdKH0',
+};
+
+const postNewScore = async (obj) => {
+  const endpoint = `${baseUrl}/games/${game.gameId}/scores`;
+  const requestInit = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  };
+
+  const response = await fetch(endpoint, requestInit);
+  const { result: newScore } = await response.json();
+
+  return newScore;
+};
+
+const getAllScores = async () => {
+  const endpoint = `${baseUrl}/games/${game.gameId}/scores`;
+
+  const response = await fetch(endpoint);
+  const { result: scores } = await response.json();
+
+  return scores;
 };
 
