51218b4a1b775f1f660ef9324eb8428c081b9382
Bahoz <bahozali50@gmail.com>
2022-07-21 15:13:17 +0300

    add the base api url and create the game with the post method using the base url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const baseUrl =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const game = {
+  name: 'tower books',
+  gameId: 'CLwRs3Mgq5HE7kyDdKH0',
+};
+
