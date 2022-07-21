f73e09914a091d88d60fed313b028d925c267b07
Bahoz <bahozali50@gmail.com>
2022-07-21 17:17:01 +0300

    implement createScoreElement() and add eventlister to the addscoreBtn to create new element and send new data to api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './style.css';
 
-const addScoreBtn = document.querySelector('add_score');
-const userInput = document.querySelector('user_input');
-const scoreInput = document.querySelector('score_input');
+const addScoreBtn = document.querySelector('#add_score');
+const userInput = document.querySelector('#user_input');
+const scoreInput = document.querySelector('#score_input');
+const userList = document.querySelector('.user_list');
 
 const baseUrl =
   'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
@@ -38,3 +39,32 @@ const getAllScores = async () => {
   return scores;
 };
 
+const createScoreElement = (user, score) => {
+  const li = document.createElement('li');
+  li.classList.add('user');
+
+  li.innerHTML = `
+              <span class="user__name">${user}</span>
+              <span class="user__score">${score}<span>
+  `;
+
+  return li;
+};
+
+addScoreBtn.addEventListener('click', () => {
+  const user = userInput.value ? userInput.value.trim() : null;
+  const score = scoreInput.value ? scoreInput.value.trim() : null;
+
+
+  if (!user || !score) return;
+
+  const data = {
+    user,
+    score,
+  };
+
+  const li = createScoreElement(user, score);
+  if (li) userList.appendChild(li);
+
+  postNewScore(data);
+});
